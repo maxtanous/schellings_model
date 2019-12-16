d95e0effe8ca37439aff72936f760dfffa2f53a4
Max Tanous <mctanous@bowdoin.edu>
2019-12-16 16:37:47 -0500

    Init node class

--- Node.py
+++ Node.py
@@ -1,7 +1,6 @@
 
 class Node:
     def __init__(self, population, educatedPop):
-        self.location = location
         self.educationValue = 0
         self.educatePop = educatedPop
         self.population = population
